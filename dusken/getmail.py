4f5aabcf9b958049f454ef9e3d199b3461a8313d
Magnus Kirø <magnuskiro@gmail.com>
2012-06-17 13:14:52 +0200

    added comments

--- dusken/getmail.py
+++ dusken/getmail.py
@@ -1,8 +1,17 @@
 from sets import Set
 import re
 
-f = open('aliases.tmp', 'r')
+"""
+The filename and opening the aliases file. 
+The aliases file is the file that maps usernames to external email addresses that we redirect email to.
+"""
+filename = "/etc/aliases"
+f = open(filename, 'r')
 
+"""
+The list of addresses that is to be found in the aliases list.
+Formating of a list is important. list = ['element', 'element', 'element']
+"""
 mlist = [
 'katrine.e.pedersen@gmail.com',
 'hanne.bronmo@gmail.com',
@@ -19,18 +28,31 @@ mlist = [
 'bluefoxofdeath@gmail.com'
 ]
 
+"""
+The result list. All the found emails are put into this list. 
+"""
 res = []
 
+"""
+regex expression for text formatting of the emails in res.
+"""
 rx = re.compile('[ \t\r\n]*')
 count = len(mlist) 
 
+# for all the lines in the aliases file
 for l in f.readlines():
+	# for all email to be found
 	for m in mlist:
+		# if the address already is an @underdusken.no address we put it in res.
 		if "@underdusken.no" in m:
 			m2 = m.replace("@underdusken.no", "");
 			if m2 in l:
 				res.append(m)
 				mlist.remove(m)
+		# if the external email is found on the line in aliases 
+		# -we format the string 
+		# -add @underdusken.no after the username
+		# - add it to res 
 		elif m in l: 
 			l = l.replace(m, "")
 			l = l + "@underdusken.no"
@@ -39,10 +61,14 @@ for l in f.readlines():
 			res.append(l)
 			mlist.remove(m)
 
+# print the found email addresses
 for a in res:
 	print a 
 
+# print the number of found addresses and the number of addresses to be found.
+# this informs the user if all emails have been found. 
 print "found:", len(res), " of:", count
 
+# print the external emails that was not found in the list.
 for m in mlist:
 	print m

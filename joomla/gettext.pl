06c5f993828d2f5cda3596bed6275b183ed5daed
Magnus Kirø <magnuskiro@gmail.com>
2013-02-28 14:19:00 +0100

    added joomla language file transaltion helper scripts. and a php mailingscript

---
+++ joomla/gettext.pl
@@ -0,0 +1,43 @@
+#! /usr/bin/perl -w
+use strict;
+
+=whatIsThis
+This is a script to read Joomla! language files and get the content from them. 
+Typically we have something like THIS_STATIC_LANGUAGE_REFERENCE="This is wha text is displayed in the program".
+Where we get the content between " and ". 
+
+Then we can use the output files content maually in translate.google.com to translate it. 
+
+Then we use the insertdata.pl script to cange the text in the language file. 
+
+=cut
+
+# open the output file for the compiled data. 
+open (OUT, '>>data.txt');
+
+#print @ARGV ."\n"; 
+
+# for all input arguments / files
+foreach my $a (@ARGV) {
+	# print the file separator to the output file. 
+	print OUT "-----new file:".$a."-----\n";
+
+# read the innput data from the files.
+open(INNPUT, $a);
+while(<INNPUT>) {
+	# get the content to translate
+	my @text = split(/"/, $_); 
+
+	# write the content to the output file. 
+	print OUT $text[1]."\n";
+
+#	print $text[1] ."\n"; 
+}
+close(INPUT);
+
+}
+
+
+close (OUT); 
+
+exit();
---
+++ joomla/inserttext.pl
@@ -0,0 +1,83 @@
+#! /usr/bin/perl -w
+use strict;
+
+=whatIsThis
+This is a script that replaces the content text of a Joomla! language file. 
+
+See gettext.pl to see how the original text is compiled.
+
+This script takes a inndata.txt file that contains the translated data. 
+And replaces the content of the filenames given on the command line. 
+
+=cut
+
+
+# get the four file arguments. 
+my @files =  @ARGV;
+my $cf = -1; # current file
+
+# initialise data array. 
+my @a = ();
+my @b = ();
+my @c = ();
+my @d = ();
+my @input = (@a,@b,@c,@d); 
+my $ct; # current text. 
+
+# read the inndata file. 
+open (INN, '<inndata.txt');
+while(<INN>) {
+	$ct = $_;
+	
+	# if there is a new file, increment the file counter to add content to the right array. 
+	if( $ct =~ m/---*/){
+		$cf++;
+		#print $ct. "match\n";
+		#print $cf."\n";
+	}
+	
+	# push read data to the array.
+	push(@{$input[$cf]}, $_);
+}
+close (INN); 
+
+
+# for all files
+$cf = 0; # reset the file counter.  
+foreach my $f (@files){
+	my $ln = 1; # line number of the current file. 
+	
+	# print $f ."\n";
+	
+	# open the output file for writing. 
+	open(OUT, ">".$f."-1");
+
+	# read the original file 
+	open(INND, "<$f");
+	while(<INND>) {
+		# split the line to get the content part. 
+		my @text = split(/"/, $_); 
+
+		# skip line if it contains nothing. 	
+		next if($text[0] =~ m/\n/);
+			
+		# swap content for translated content. 
+		$text[1] = ${$input[$cf]}[$ln];
+		# remote newline at end of line to make the output file cleaner. 
+		$text[1] =~ s/\n//g;
+
+		# write the new line to the output file. 
+		print OUT $text[0]."\"".$text[1]."\"\n";
+		
+		# increment the line counter. 
+		$ln++; 
+	}
+	close(INND);
+
+	close(OUT);
+
+	# increment the file counter to keep track of which file we're working on.
+	$cf++;
+}
+
+exit();

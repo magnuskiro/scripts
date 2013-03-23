c8c5ca261d9dd2bda61f286aaf6e4d21068001a3
Magnus Kirø <magnuskiro@gmail.com>
2013-03-23 12:39:01 +0100

    script fro running oneliner sql statements to a predefined server, from the command line

---
+++ dbSQLonLine.pl
@@ -0,0 +1,63 @@
+#! /usr/bin/perl -w
+use strict;
+use DBI;
+
+my %object;
+my $sth;
+#my $contact_sth = $dbh->prepare("INSERT INTO domains VALUES(?,?,?,?,?,?)");
+
+print "hore----------------\n";
+
+#	get query from input.  
+my $sql = "";
+foreach my $a (@ARGV){
+	$sql = $sql.$a;
+	#print $a."\n"; 
+}
+print $sql."\n";
+
+die unless defined $sql;
+
+exit();
+
+my $dbh;
+my $host = 'underdusken.no';
+my $db = 'barweb_dev';
+my $user = 'barweb_dev';
+my $pass = 'barweb_dev';
+
+# 	create connection object.  
+#	DBI:dbType:dbName:hostToConnectTo
+my $connect = 'DBI:mysql:'.$db.':host='.$host;
+
+#	connect to db. 
+$dbh = DBI->connect($connect, $user, $pass) || die "Could not connect to database: $DBI::errstr";
+
+# 	set UTF8
+#$dbh->{'mysql_enable_utf8'} = 1;
+
+# 	prepare sql query. 
+my $search_sth  = $dbh->prepare($sql); 
+# 	execte sql query. 
+$search_sth->execute();
+
+my $a = "
+SELECT firstname, surname FROM person INNER JOIN vakt ON person.ID = vakt.ATTENDAND_ID OR person.ID = vakt.SECONDATTENDAND_ID WHERE vakt.TIME > '2013-01-01 00:00:59' ORDER BY person.firstname
+";
+
+my $count = 0;
+# 	fetching all the result rows from the executed query. 
+while ( my  @row = $search_sth->fetchrow_array ) {
+	# 	for all fields in the sql record. 
+	foreach my $s (@row){
+		if(defined $s){		
+			print $s;
+		}else {
+			print " ";
+		}
+		print "|"; 
+	}
+	print "\n";
+	$count ++;  
+}
+print "count = ".$count."\n"; 

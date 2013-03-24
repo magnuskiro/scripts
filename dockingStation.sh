ac55aa4f64bb4c7447b4f22813266fdb4ab78fb1
Magnus Kirø <magnuskiro@gmail.com>
2013-03-24 16:39:23 +0100

    more work on docking script and som more work on setup script

--- dockingStation.sh
+++ dockingStation.sh
@@ -2,6 +2,11 @@
 
 # script to configure my laptop to use the setup I have at home.
 
+# TODO dock only on local network.  
+# if ip is in range 192.168.1.0/24
 synergyc -n kiro-tp 192.168.1.102
 
+# TODO set screen resolutions for multiple docking screens
+
+# TODO add different profiles for different docking places. (undocked, home, school, work)
 

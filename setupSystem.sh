8dc04bcdb7afa4d68b891f356fbf0d2a6d3d4568
Magnus Kirø <magnuskiro@gmail.com>
2013-03-28 02:56:26 +0100

    fixed the git repo links

--- setupSystem.sh
+++ setupSystem.sh
@@ -1,11 +1,11 @@
+#!/bin/sh
 # the hope is to copy and pase this into the command line to make this work out nicely. 
+
+################################################
+#general configuration cross linux machines. 
 
 # Static variables
 conf="~/repos/configs"
-#bashrc=$1
-
-bashrc="~/.bashrc"
-
 
 # create folders
 mkdir ~/repos
@@ -13,30 +13,46 @@ mkdir ~/ntnu
 mkdir ~/dusken
 
 # install packages
-apt-get install git
+apt-get install git vim exuberant-ctags libparse-exuberantctags-perl ack-grep
 
 # create ssh key for git. 
 cat ~/.ssh/id_rsa.pub 
-read -p "Press any key to continue... "
+read -p "The manual step, put ssh key into github.com then Press any key to continue... "
 
-# clone projects. 
-# TODO add repository cloning 
-git clone git@github.com/magnuskiro/configs.git $conf
+# clone projects.
+cd repos 
+# TODO add repository cloning
+	# create some kind of for loop with an array. 
+repo="configs"
+git clone git@github.com:magnuskiro/$repo.git
+repo="scripts"
+git clone git@github.com:magnuskiro/$repo.git
+
+cd
 
 # create symlinks. 
 # TODO fix the rest of the symlinks.
-ln -s $conf/.vim .vim
 
+# vim 
+rm -rf ~/.vim && ln -s $conf/.vim .vim
+rm ~/.vimrc && ln -s $conf/.vimrc .vimrc
 
-rm -rf ~/.gitconfig && ln -s $conf/.gitconfig ~/.gitconfig
+# profile
+rm ~/.profile && ln -s $conf/.profile ~/.profile
 
-# append to system configs.
-# echo 'hei' >> test
+# bashrc
+rm ~/.bashrc && ln -s $conf/.bashrc ~/.bashrc
 
-# TODO figure out howto add to files as sudo. 
-echo "source $conf/aliases.bashrc" >> $bashrc
-echo "source $conf/color.bashrc" >> $bashrc
+# git
+rm ~/.gitconfig && ln -s $conf/.gitconfig ~/.gitconfig
 
+################################################
+#Profiles 
+# different system specific changes. 
+## --server
+
+## --x201
 # thinkpad config.
-cd /usr/share/X11/xorg.conf.d/ & sudo ln -s $conf/20-thinkpad.conf 20-thinkpad.conf
- 
+#cd /usr/share/X11/xorg.conf.d/ & sudo ln -s $conf/20-thinkpad.conf 20-thinkpad.conf
+echo "dock" >> ~/.bashrc
+

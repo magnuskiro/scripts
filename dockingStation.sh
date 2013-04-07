89e2d8cd9c340af11e34f8d1bed0671a0d835dbc
Magnus Kirø <magnuskiro@gmail.com>
2013-04-07 22:24:40 +0200

    somewhat fixed screen resolution and external screen for dockingstation

--- dockingStation.sh
+++ dockingStation.sh
@@ -2,19 +2,22 @@
 
 # script to configure my laptop to use the setup I have at home.
 
-# TODO dock only on local network.  
 # if ip is in range 192.168.1.0/24
 if ! pidof synergyc ; then
+
+	# screen setup
+	xrandr --output LVDS1 --primary
+	xrandr --output VGA1 --auto --left-of LVDS1 
+
+	# have to connect to synergy after setting the scrren resolutions. 	
 	# connects stationary keyboard and mouse. 
 	synergyc -n kiro-tp 193.71.111.23
-
-	# screen setup 
 else 
 	# disconnects keyboard and mouse. 
 	killall synergyc
 
 	# reset screen resolutions.
-	
+	xrandr --output VGA1 --off
 fi
 
 

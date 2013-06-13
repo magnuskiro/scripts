7cf217fb430bd19e679c9164dfbb50c8d8ccf8a1
Magnus Kirø <magnuskiro@gmail.com>
2013-06-13 20:32:05 +0200

    changed the way the status mail script gets my current ip.

---
+++ echoAllIPsFromFile.pl
@@ -0,0 +1,21 @@
+#!/shared/perl
+
+# get file to search from the first input argument
+my $arg = $ARGV[0];
+
+# open current file
+open(FILE, $arg) or die $!;
+
+# for all files in the line
+while (<FILE>) {
+	# if the line contains an ipv4  address
+	if(m/\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/){
+		# print that ipv4 address
+		print $1, "\n";
+	} 
+
+}
+
+# close file. 
+close(FILE);
+

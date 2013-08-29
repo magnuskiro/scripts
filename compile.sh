d36ed48d1e9f86bd42112e61927549da2dba4db6
Magnus Kirø <magnuskiro@gmail.com>
2013-08-29 15:54:41 +0200

    updated the compile latex compile script

--- compile.sh
+++ compile.sh
@@ -18,14 +18,15 @@ The first being the document and the second being the log output file.
 # Specifics and explenations.
 compiling twice because once might not do the trick. 
 
-# TODO 
-
-TODO: add functionality for opening the pdf document in evince. 
-"evince $doc &" 
-
-TODO: add file changed listening. On write, recompile document. 
-
 END
+
+# no arguments --> exit. 
+if [ $# -eq 0 ];
+then
+	echo "ERROR: Insufficient arguments"
+	echo "Use: 'compile file.tex file.log'"
+	exit 1
+fi 
 
 echo "Pidof this: "$$
 name=`echo "$1" | cut -d'.' -f1`
@@ -34,44 +35,45 @@ evincePID=0
 
 while event=$(inotifywait -e MOVE_SELF,CLOSE_WRITE $1 > /dev/null 2>&1)
 do
+# start wait for tex file change event loop.
+	
+	# if no log file is specified we use the standard log.
+    if [ -z "$2" ];
+    then
+    	pdflatex -interaction nonstopmode $1 > $name.log
+    	pdflatex -interaction nonstopmode $1 > $name.log
+    else
+    	pdflatex -interaction nonstopmode $1 > $2
+    	pdflatex -interaction nonstopmode $1 > $2
+    fi
+    
+    # removes garbage after compile. 
+    rm -f *.toc *.aux *.out  *.snm *.nav *.dvi *.lof $name.log 
+    
+    # get the  wordcount for the current document. 
+    wc=`pdftotext $pdf - | wc -w`
+    
+    echo "Compiled '$1' with $wc words"
+    
+    
+    ### start evince to display the pdf.
+    # if the pdf file exists and the pid is 0.
+    # we assume that when the pid!=0 evince is running
+    # drawback is that we don't kill the evince process.  
+    
+    #file="/proc/"$evincePID
+    #if [ -f $pdf ] && [ ! -f $file ];
+    if [ -f $pdf ] && [ $evincePID -eq 0 ];
+    then
+        evince $pdf &
+        evincePID=$!
+        echo "EvincePId: "$evincePID
+    fi
+    ### end evince start code. 
 
-if [ -n "$1" ] && [ -n "$2" ];
-then
-	pdflatex -interaction nonstopmode $1 > $2
-	pdflatex -interaction nonstopmode $1 > $2
-elif [ "$1" != "" ];
-then
-	pdflatex -interaction nonstopmode $1 > log
-	pdflatex -interaction nonstopmode $1 > log
-else
-	echo "usage: ./compile file.tex file.log"
-	exit 1
-fi
-
-# removes garbage after compile. 
-rm -f log *.toc *.aux *.out  *.snm *.nav *.dvi *.lof 
-
-# get the  wordcount for the current document. 
-wc=`pdftotext $pdf - | wc -w`
-
-echo "Compiled '$1' with $wc words"
+# end while loop.
+done  
 
 
-### start evince to display the pdf.
-# if the pdf file exists and the pid is 0.
-# we assume that when the pdi!=0 evince is running
-# drawback is that we don't kill the evince process.  
-
-#file="/proc/"$evincePID
-#if [ -f $pdf ] && [ ! -f $file ];
-if [ -f $pdf ] && [ $evincePID -eq 0 ];
-then
-    evince $pdf &
-    evincePID=$!
-    echo "EvincePId: "$evincePID
-fi
-### end evince start code. 
-
-done
 
 exit 1

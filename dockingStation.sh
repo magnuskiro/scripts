2eb67e662c50c8e24d2b15087a530686e4e70b97
Magnus Kirø <magnuskiro@gmail.com>
2013-09-09 03:34:58 +0200

    updated the docking script to use different profiles, This way I can use a rojector and a docking station etc

--- dockingStation.sh
+++ dockingStation.sh
@@ -2,28 +2,49 @@
 
 # script to configure my laptop to use the setup I have at home.
 
-# if ip is in range 192.168.1.0/24
-if ! pidof synergyc ; then
+if [ -z "$1" ];
+then
 
-	# screen setup
-	xrandr --output LVDS1 --primary
-	xrandr --output VGA1 --auto --left-of LVDS1 
+# standard docking
+    if ( xrandr | grep "VGA1 connected" > /dev/null );
+    then # if there is a vga1 screen connected.
+    # disengade docking. 
+     
+    	echo "INFO-- Laptop only mode." 
+    	# disconnects keyboard and mouse. 
+        #killall synergyc
+    
+        # reset screen resolutions.
+    	xrandr --output VGA1 --off --output LVDS1 --auto
+    
+    else # if there are no vga1 screen connected.
+    # engade docking 
+    
+    	# screen setup
+   		xrandr --output LVDS1 --auto --output VGA1 --primary --auto --left-of LVDS1
+    	echo "INFO-- Docking mode."
+    
+    	# have to connect to synergy after setting the scrren resolutions. 	
+    	# connects stationary keyboard and mouse. 
+    	#synergyc -n kiro-tp 193.71.111.23
+    fi
 
-	# have to connect to synergy after setting the scrren resolutions. 	
-	# connects stationary keyboard and mouse. 
-	synergyc -n kiro-tp 193.71.111.23
 else 
-	# disconnects keyboard and mouse. 
-	killall synergyc
-
-	# reset screen resolutions.
-	xrandr --output VGA1 --off
-	xrandr --output LVDS1 --auto
+# projector mode.
+	if [ "$1" = "-p" ];
+	then
+		echo "INFO-- Projector mode."
+		xrandr --output LVDS1 --auto --output VGA1 --auto --same-as LVDS1
+	elif [ "$1" = "-d" ];
+	then
+    	echo "INFO-- Docking mode."
+   		xrandr --output LVDS1 --auto --output VGA1 --primary --auto --left-of LVDS1
+	else
+		echo "ERROR: Not a valid param. Use '-p' or nothing"
+	fi
+	
 fi
-
-
 
 # TODO set screen resolutions for multiple docking screens
 
-# TODO add different profiles for different docking places. (undocked, home, school, work)
-
+exit 1

d4eee99492742c1a9dacf4f01c8710f8fd870c99
Magnus Kirø <magnuskiro@gmail.com>
2013-10-22 22:23:28 +0200

    get mail from file script update, it now works a bit better than before

--- getAllEmailsFromFile.pl
+++ getAllEmailsFromFile.pl
@@ -1,25 +1,37 @@
 #!/usr/bin/perl
 
+# Usage:
+# arg1= input file. 
+# @ARGV = the extensions that we don't want. 
+
 # get file to search from the first input argument
-my $arg = $ARGV[0];
-my $arg2 = $ARGV[1];
+my $arg = shift @ARGV;
+my $count = 0;
 
 # open current file
 open(FILE, $arg) or die $!;
 
 # for all lines in the file
 while (<FILE>) {
-	# TODO fix. 
-	if ($arg2){
-    	# if the line contains en email address
-    	if(m/([-A-Za-z0-9\.]*@[A-Za-z0-9\.]*\.[a-z]{2,4})/){
-    		# print the address
-    		print $1, "\n";
+   	# if the line contains en email address
+	if(m/([-A-Za-z0-9\.]*@[A-Za-z0-9\.]*\.[a-z]{2,4})/){
+		# todo
+		# if the email address contains something don't use it. 
+		# if ($1 ~= m/$arg2/){
+		foreach my $item (@ARGV){
+			# TODO fix so that we only prints once for all the items we don't
+			# want. 
+			if (not m/$item/){
+    			# print the address
+				$count += 1;
+    			print $1, "\n";
+			}
     	} 
 	}
-
 }
 
 # close file. 
 close(FILE);
+
+print $count."\n";
 

f01d9aba0cd57af29bb321ca3e593bb8f210d647
Magnus Kirø <magnuskiro@gmail.com>
2013-12-16 13:57:40 +0100

    added shell script template

---
+++ shellSkeleton.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+: <<'END'
+
+Comments, requirements and userguide here. 
+
+## Description
+This is a bash script template. To quickly create good and powerfull scripts.  
+
+## Requirements
+You need these packages or programs for this script to work. 
+
+## Use: 
+* one parameter
+bash_program -a
+* multiple parameters without extra input.
+bash_program -bac
+* multiple parameters with input. 
+bash_program -a a-param-input -b another_input_value
+
+END
+
+Function () {
+    a="variable"
+    echo $a printed
+}
+
+anotherOne () {
+	echo "bach functions"
+}
+
+# Input gathering. 
+while getopts "ab:c:" opt; do
+  case $opt in
+	# -a
+    a)  
+		# here we do stuffs 
+		echo "-a was specified"  
+	;;
+	# -b 
+    b)  
+		# here we do something else.
+		echo "arg given to b is $OPTARG" 
+	;;
+	# -c
+    c)  
+		# and another one. 
+		echo "-c was specified"  
+	;;
+	# invalid options 
+    \?) echo "Invalid option: -$OPTARG" >&2 ;;
+  esac
+done 
+
+exit()
+

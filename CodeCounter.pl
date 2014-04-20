08c9734a87c2cc2669a40bd68bb388b563b18567
Magnus Kirø <magnuskiro@gmail.com>
2014-04-20 20:06:11 +0200

    finished code counting script

---
+++ CodeCounter.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Cwd;
+
+# if we get no file extension crash
+die("No file extension given!") unless $ARGV[0];
+
+sub countFolder {
+	# get the directory to count lines of code. 
+	my $dir = shift @_;
+	# get the file extensions we want to count lines in. 
+	my @extensions = @_;
+
+	#print "-- ".$dir." ".$extensions[0]." ".@extensions."\n";
+
+	my $count = 0;
+
+	# init the directiory variable. 
+	my $mydir; 
+    # Open the given folder for reading.
+    opendir ($mydir, $dir) or die $!;
+ 
+    # find all files in the folder. 
+    while (my $file = readdir($mydir)) {
+		# for all the file extensions	
+    	for my $extension (@extensions){
+			# skip . and .. files. 
+			if ( $file =~ m/^\.+/ ){ next }; 
+			# if directory: do recursion
+			if ( -d $file ) {
+				# add the line count of a folder to the total.
+				$count += countFolder($dir.$file."/", @extensions);
+			}
+			# if wanted file extension
+        	if ( $file =~ m/\.$extension$/ ){
+        		# if we get line count from wc 
+				if (`wc -l $dir$file` =~ m/(^\d\d)/ ){
+					# add the number to the total count. 
+					$count += $1;
+				} else {
+					$count += 0;
+				}
+        	} 
+    	}
+    }
+	# close the directory.
+    closedir($mydir);
+
+	# return the code lines count. 
+	return $count; 
+}
+
+# input the current working directory and array of file extensions 
+print "Lines of '@ARGV' Code: ".countFolder(getcwd."/", @ARGV) . "\n";
+

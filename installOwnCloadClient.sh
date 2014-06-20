f2bdf2c0a55547f5e3d2e76b1dc6764bb499358f
Magnus Kirø <magnuskiro@gmail.com>
2014-06-20 22:19:55 +0200

    started work on owncloud install script

--- installOwnCloadClient.sh
+++ installOwnCloadClient.sh
@@ -1,1 +1,5 @@
 echo "INFO - OwnCloud Client install"
+
+wget http://download.opensuse.org/repositories/isv:ownCloud:community/Debian_6.0/Release.key 
+sudo apt-key add - < ./Release.key 
+rm ./Release.key

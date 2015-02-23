f31a2a16f1b593f94889a28c215e27cc1c3c3369
Magnus Kirø <magnuskiro@gmail.com>
2015-02-23 17:40:01 +0100

    script updates

--- perl/execSQLfromInputStoreToFile.pl
+++ perl/execSQLfromInputStoreToFile.pl
@@ -1,60 +1,67 @@
 #! /usr/bin/perl -w
 use strict;
+use utf8;
 use DBI;
+binmode(STDOUT, ":utf8");
 
-my %object;
-my $sth;
-#my $contact_sth = $dbh->prepare("INSERT INTO domains VALUES(?,?,?,?,?,?)");
-
+# usage:
+# perl script.pl sql statement;
 
 #	get query from input.  
 my $sql = "";
 foreach my $a (@ARGV){
-	$sql = $sql.$a;
+	$sql = $sql.$a." ";
 	#print $a."\n"; 
 }
+$sql = $sql.";";
 print "query: '".$sql."'\n";
 
 die unless defined $sql;
 
+print "INFO -- setting connection info\n";
 my $dbh;
-my $host = 'underdusken.no';
-my $db = 'barweb_dev';
-my $user = 'barweb_dev';
-my $pass = 'barweb_dev';
+my $host = '';
+my $db = '';
+my $user = '';
+my $pass = '';
 
 # 	create connection object.  
 #	DBI:dbType:dbName:hostToConnectTo
 my $connect = 'DBI:mysql:'.$db.':host='.$host;
 
 #	connect to db. 
+print "INFO -- connecting to DB\n";
 $dbh = DBI->connect($connect, $user, $pass) || die "Could not connect to database: $DBI::errstr";
 
 # 	set UTF8
-#$dbh->{'mysql_enable_utf8'} = 1;
+$dbh->{'mysql_enable_utf8'} = 1;
 
 # 	prepare sql query. 
+print "INFO -- prepearing statement\n";
 my $search_sth  = $dbh->prepare($sql); 
 # 	execte sql query. 
 $search_sth->execute();
 
-my $a = "
-SELECT firstname, surname FROM person INNER JOIN vakt ON person.ID = vakt.ATTENDAND_ID OR person.ID = vakt.SECONDATTENDAND_ID WHERE vakt.TIME > '2013-01-01 00:00:59' ORDER BY person.firstname
-";
+print "INFO -- executing fetch\n";
+
+my $filename = "internNyheter.txt";
+open (FILE, ">> $filename") || die "problem opening $filename\n";
 
 my $count = 0;
 # 	fetching all the result rows from the executed query. 
-while ( my  @row = $search_sth->fetchrow_array ) {
+while ( my @row = $search_sth->fetchrow_array ) {
 	# 	for all fields in the sql record. 
 	foreach my $s (@row){
 		if(defined $s){		
-			print $s;
+			print FILE $s;
 		}else {
-			print " ";
+			print FILE " ";
 		}
-		print "\t"; 
+		print FILE "\t"; 
 	}
-	print "\n";
+	print FILE "\n";
+	print FILE "!!---------------------------!!\n";
 	$count ++;  
+	#exit;
 }
 print "count = ".$count."\n"; 
--- playInputSoundOnOtherOutputDevice.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gst-launch pulsesrc ! pulsesink & 
-

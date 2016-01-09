0dd664b001d62d44e236253c5a5d293087282712
Magnus Kirø <magnuskiro@gmail.com>
2016-01-09 16:45:52 +0100

    get_screen_dimensions.lua: fixed digit matching, now getting 1 or more digits, not 4 only.

--- get_screen_dimensions.lua
+++ get_screen_dimensions.lua
@@ -1,13 +1,13 @@
 #!/usr/bin/lua 
 
--- get the dimensions of the total screen resolution. ex: 3200x1080
+-- get the dimensions of the total screen resolution. example: 3200x1080
 local screen_dimension = io.popen("xdpyinfo | grep \"dimensions:\" | grep -oP \"\\d+x\\d+\"")
 
 local dimensions = {}
 -- first line is pixels, second line is mm.
 for l in screen_dimension:lines() do
     --print(l)
-    for dim in string.gmatch(l, "%d%d%d%d") do
+    for dim in string.gmatch(l, "%d+") do
         --print(dim)
         table.insert(dimensions,dim)
     end

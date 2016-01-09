b2ae3af2061ea0859bbb802340293517ee2b1bff
Magnus Kirø <magnuskiro@gmail.com>
2016-01-09 16:42:20 +0100

    Added lua script to get screen dimensions.

---
+++ get_screen_dimensions.lua
@@ -0,0 +1,19 @@
+#!/usr/bin/lua 
+
+-- get the dimensions of the total screen resolution. ex: 3200x1080
+local screen_dimension = io.popen("xdpyinfo | grep \"dimensions:\" | grep -oP \"\\d+x\\d+\"")
+
+local dimensions = {}
+-- first line is pixels, second line is mm.
+for l in screen_dimension:lines() do
+    --print(l)
+    for dim in string.gmatch(l, "%d%d%d%d") do
+        --print(dim)
+        table.insert(dimensions,dim)
+    end
+    break -- we only want the first line.
+end
+
+print("----")
+print(dimensions[1])
+print(dimensions[2])

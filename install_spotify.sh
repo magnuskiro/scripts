43816259ac73daf48697c6b7ce1c93148b6c081e
Magnus Kirø <magnuskiro@gmail.com>
2020-09-08 09:36:57 +0200

    apt simplification

--- install_spotify.sh
+++ install_spotify.sh
@@ -16,7 +16,7 @@ InstallSpotify () {
     curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
     echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-    sudo apt-get update && sudo apt-get install spotify-client
+    sudo apt update && sudo apt install -y spotify-client
 }
 
 echo "-- INFO - Installing Spotify"

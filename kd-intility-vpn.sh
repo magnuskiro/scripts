e0146998242dc6c6dd6f9bf755251372a682bb5a
Magnus Kirø <magnuskiro@gmail.com>
2022-01-06 09:42:48 +0100

    kd vpn script update: now more generalised

--- kd-intility-vpn.sh
+++ kd-intility-vpn.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/expect
 
+####
+#   depends on 'expect', `apt install expect`
+####
+
+set user "magnus.kiro@klavenessdigital.com"
+
 proc slurp {file} {
     set fh [open $file r]
     set ret [read $fh]
@@ -7,16 +13,17 @@ proc slurp {file} {
     return $ret
 }
 
-set pw [slurp /home/kiro/kd/vpn.secret]
+# before use ensure that this file contains your AD-pw
+set pw [slurp $env(HOME)/kd/vpn.secret]
 
 # prints the pw / for debug
-puts -nonewline $pw 
+#puts -nonewline $pw 
 
 #spawn openconnect --csd-user=nobody --csd-wrapper=/usr/lib/openconnect/csd-post.sh vpn.intility.no
 spawn /opt/cisco/anyconnect/bin/vpn connect vpn.intility.no
 
 expect "Username:"
-send "magnus.kiro@klavenessdigital.com\n"
+send "$user\n"
 expect "Password:"
 send "$pw\n"
 expect "Answer: "

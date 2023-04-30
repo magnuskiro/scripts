3e23d3bb3bbc4ae6bb4f8c738d409ceae0ad3e57
Magnus Kirø <magnuskiro@gmail.com>
2023-04-30 12:19:16 +0200

    install discord script

---
+++ install_discord.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+: <<'END'
+
+Works on debian based Linux.
+
+Auto install command: 
+
+wget \
+https://raw.githubusercontent.com/magnuskiro/scripts/master/installDiscord.sh \
+&& chmod 755 ./installDiscord.sh && ./installDiscord.sh && rm ./installDiscord.sh 
+
+END
+
+InstallDiscord () {
+  wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
+  sudo apt install ./discord.deb -y
+}
+
+echo "-- INFO - Installing Discord"
+InstallDiscord
+

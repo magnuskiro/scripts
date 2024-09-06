cc4e9158328791391f90c7997a695587cdbdb000
Magnus Kirø <magnuskiro@gmail.com>
2024-09-06 09:59:10 +0200

    addig xautolock to Setup script

--- Setup.sh
+++ Setup.sh
@@ -43,7 +43,7 @@ PackageInstall () {
     	
 	packages="exuberant-ctags libparse-exuberantctags-perl screen
 filezilla texlive lmodern texlive-extra-utils inotify-tools openssh-server eog
-vlc maven lmodern i3 i3lock pavucontrol arandr expect docker.io docker-compose feh" 
+vlc maven lmodern i3 i3lock xautolock pavucontrol arandr expect docker.io docker-compose feh"
 
 	echo "-- INFO - Installing extra packages"
 	sudo apt install -y $packages
